dbe62fd4331437c9161dc3ddfabcf6777bc3511f
Natalie-Yankovskaya <natalie.yankovskaya0209@gmail.com>
2021-06-25 17:37:29 +0300

    Added hosted server

--- src/services/api.js
+++ src/services/api.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 
 export default () => {
   return axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://apz-project.herokuapp.com',
     withCredentials: false,
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json"
     }
   });
-} 
+}  
